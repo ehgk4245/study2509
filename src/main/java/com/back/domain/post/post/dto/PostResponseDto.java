e6bdc3116b23d6a2a7a689a8631ac1a24c865c87
ehgk4245 <ehgkrk14@naver.com>
2025-09-03 15:18:19 +0900

    DTO record로 변환

--- src/main/java/com/back/domain/post/post/dto/PostResponseDto.java
+++ src/main/java/com/back/domain/post/post/dto/PostResponseDto.java
@@ -1,27 +1,23 @@
 package com.back.domain.post.post.dto;
 
 import com.back.domain.post.post.entity.Post;
-import lombok.Builder;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
-@Builder
-public class PostResponseDto {
-    private Long id;
-    private String subject;
-    private String body;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
-
+public record PostResponseDto(
+        Long id,
+        String subject,
+        String body,
+        LocalDateTime createdDate,
+        LocalDateTime modifiedDate
+) {
     public static PostResponseDto from(Post post) {
-        return PostResponseDto.builder()
-                .id(post.getId())
-                .subject(post.getTitle())
-                .body(post.getContent())
-                .createdDate(post.getCreateDate())
-                .modifiedDate(post.getModifyDate())
-                .build();
+        return new PostResponseDto(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getCreateDate(),
+                post.getModifyDate()
+        );
     }
 }
